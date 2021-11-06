ab0c09ce26f7d16cd70188cc3cc481a7f6354be8
eight_y_88 <jadec.eight@gmail.com>
2021-11-06 19:35:01 +0900

    Valut(BankSupportなし)

--- src/main/java/net/monkeyfunky/devteam/economy/Economy.java
+++ src/main/java/net/monkeyfunky/devteam/economy/Economy.java
@@ -1,6 +1,11 @@
 package net.monkeyfunky.devteam.economy;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -14,6 +19,8 @@ public final class Economy extends JavaPlugin {
 
     private static EconomyAPI api;
     private Connection connection;
+
+    private VaultEconomy economy;
 
     @Override
     public void onEnable() {
@@ -32,6 +39,31 @@ public final class Economy extends JavaPlugin {
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        }
+
+        if (economy == null) {
+            Plugin vault = getServer().getPluginManager().getPlugin("Vault");
+            if (vault != null) {
+                if (vault.isEnabled()) {
+                    if (economy != null) {
+                        return;
+                    }
+
+                    economy = new VaultEconomy(api);
+                    getServer().getServicesManager().register(net.milkbowl.vault.economy.Economy.class, economy, this, ServicePriority.Normal);
+                } else {
+                    getServer().getPluginManager().registerEvents(new Listener() {
+                        @EventHandler(ignoreCancelled = true)
+                        public void onPluginEnable(PluginEnableEvent e) {
+                            if (!e.getPlugin().getName().equals("Vault")) {
+                                return;
+                            }
+                            getServer().getServicesManager().register(net.milkbowl.vault.economy.Economy.class, economy, Economy.getInstance(), ServicePriority.Normal);
+                            PluginEnableEvent.getHandlerList().unregister(this);
+                        }
+                    }, this);
+                }
+            }
         }
     }
 
